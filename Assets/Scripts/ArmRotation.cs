682828c4c0ee0b1f00eef5b7419c5ed449f84f62
Igor <kwadratowamasakra@gmail.com>
2022-12-10 18:29:09 +0100

    Fixed arm rotation while in pause menu

--- Assets/Scripts/ArmRotation.cs
+++ Assets/Scripts/ArmRotation.cs
@@ -10,10 +10,15 @@ public class ArmRotation : MonoBehaviour
 
     void Update()
     {
+        if ( !PauseMenu.GameIsPaused )
+        {
+
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         difference.Normalize();
 
         float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+
+        }
     }
 }
